433b31ea86b519a0123b5c9a575549238826dcac
Eliza Weisman <eliza@buoyant.io>
2020-06-16 14:05:19 -0700

    rust: Upgrade from tower-grpc to tonic (#39)
    Tonic (https://crates.io/crates/tonic) is, essentially, the Tokio 0.2
    world's version of `tower-grpc`. This change updates the rust crate
    to these newer dependencies.

--- rs/build.rs
+++ rs/build.rs
@@ -1,27 +1,29 @@
-extern crate tower_grpc_build;
+use std::error::Error;
 
-fn main() {
-    build_control();
+fn main() -> Result<(), Box<dyn Error>> {
+    build_control()?;
+    Ok(())
 }
 
-fn build_control() {
+fn build_control() -> Result<(), Box<dyn Error>> {
     let iface_files = &[
         "../proto/destination.proto",
         "../proto/http_types.proto",
         "../proto/identity.proto",
         "../proto/net.proto",
-        "../proto/tap.proto"
+        "../proto/tap.proto",
     ];
     let dirs = &["../proto"];
 
-    tower_grpc_build::Config::new()
-        .enable_client(true)
-        .enable_server(true)
-        .build(iface_files, dirs)
-        .unwrap();
+    tonic_build::configure()
+        .build_client(true)
+        .build_server(true)
+        .compile(iface_files, dirs)?;
 
     // recompile protobufs only if any of the proto files changes.
     for file in iface_files {
         println!("cargo:rerun-if-changed={}", file);
     }
+
+    Ok(())
 }
