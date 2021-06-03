322cd8de0cc99cf8d4740d80aa057fd4de16a216
Oliver Gould <ver@buoyant.io>
2021-06-03 10:56:40 -0700

    rust: Add per-API feature flags (#59)
    The proxy-api crate always builds servers and clients for all gRPC APIs,
    but practically clients usually only intend to pull in a single
    client/server for a single API.
    
    This change introduces cargo features which are honored by `build.rs` to
    limit which files and functionality are generated.
    
    This change also fixes the `quickcheck::Arbitrary` implementations and
    splits them into their respective modules.
    
    CI is updated to exercise individual features in clippy and to use Rust
    v1.52.1.
    
    Finally, this change updates all cargo dependencies en masse.

--- rs/build.rs
+++ rs/build.rs
@@ -1,29 +1,42 @@
 use std::error::Error;
 
+const DIR: &str = "../proto";
+const PROTOS: [&str; 5] = ["destination", "http_types", "identity", "net", "tap"];
+
 fn main() -> Result<(), Box<dyn Error>> {
-    build_control()?;
+    let files = PROTOS
+        .iter()
+        .filter_map(|p| feature_proto(DIR, *p))
+        .collect::<Vec<_>>();
+    if files.is_empty() {
+        return Err("no interfaces enabled".into());
+    }
+
+    let build_client = std::env::var_os("CARGO_FEATURE_CLIENT").is_some();
+    let build_server = std::env::var_os("CARGO_FEATURE_SERVER").is_some();
+    let build_arbitrary = std::env::var_os("CARGO_FEATURE_ARBITRARY").is_some();
+    if !build_client && !build_server && !build_arbitrary {
+        return Err(
+            "either the `client`, `server`, or `arbitrary` features must be enabled".into(),
+        );
+    }
+    tonic_build::configure()
+        .build_client(build_client)
+        .build_server(build_server)
+        .compile(&*files, &[DIR.to_string()])?;
+
+    // recompile protobufs only if any of the proto files changes.
+    for file in files.iter() {
+        println!("cargo:rerun-if-changed={}", file);
+    }
     Ok(())
 }
 
-fn build_control() -> Result<(), Box<dyn Error>> {
-    let iface_files = &[
-        "../proto/destination.proto",
-        "../proto/http_types.proto",
-        "../proto/identity.proto",
-        "../proto/net.proto",
-        "../proto/tap.proto",
-    ];
-    let dirs = &["../proto"];
-
-    tonic_build::configure()
-        .build_client(true)
-        .build_server(true)
-        .compile(iface_files, dirs)?;
-
-    // recompile protobufs only if any of the proto files changes.
-    for file in iface_files {
-        println!("cargo:rerun-if-changed={}", file);
+fn feature_proto(dir: &str, name: &str) -> Option<String> {
+    let env = format!("CARGO_FEATURE_{}", name.to_uppercase());
+    if std::env::var(env).is_ok() {
+        Some(format!("{}/{}.proto", dir, name.to_lowercase()))
+    } else {
+        None
     }
-
-    Ok(())
 }
