5242541dfb2b713ed26701002927602c3328b866
katelyn martin <kate@buoyant.io>
2025-03-26 00:00:00 +0000

    chore(gen): update generated code
    this commit checks in changes from running `cargo run --example=gen`.
    
    Signed-off-by: katelyn martin <kate@buoyant.io>

--- src/gen/io.linkerd.proxy.inbound.rs
+++ src/gen/io.linkerd.proxy.inbound.rs
@@ -345,7 +345,7 @@ pub mod inbound_server_policies_client {
     }
     impl<T> InboundServerPoliciesClient<T>
     where
-        T: tonic::client::GrpcService<tonic::body::BoxBody>,
+        T: tonic::client::GrpcService<tonic::body::Body>,
         T::Error: Into<StdError>,
         T::ResponseBody: Body<Data = Bytes> + std::marker::Send + 'static,
         <T::ResponseBody as Body>::Error: Into<StdError> + std::marker::Send,
@@ -366,13 +366,13 @@ pub mod inbound_server_policies_client {
             F: tonic::service::Interceptor,
             T::ResponseBody: Default,
             T: tonic::codegen::Service<
-                http::Request<tonic::body::BoxBody>,
+                http::Request<tonic::body::Body>,
                 Response = http::Response<
-                    <T as tonic::client::GrpcService<tonic::body::BoxBody>>::ResponseBody,
+                    <T as tonic::client::GrpcService<tonic::body::Body>>::ResponseBody,
                 >,
             >,
             <T as tonic::codegen::Service<
-                http::Request<tonic::body::BoxBody>,
+                http::Request<tonic::body::Body>,
             >>::Error: Into<StdError> + std::marker::Send + std::marker::Sync,
         {
             InboundServerPoliciesClient::new(InterceptedService::new(inner, interceptor))
@@ -570,7 +570,7 @@ pub mod inbound_server_policies_server {
         B: Body + std::marker::Send + 'static,
         B::Error: Into<StdError> + std::marker::Send + 'static,
     {
-        type Response = http::Response<tonic::body::BoxBody>;
+        type Response = http::Response<tonic::body::Body>;
         type Error = std::convert::Infallible;
         type Future = BoxFuture<Self::Response, Self::Error>;
         fn poll_ready(
@@ -675,7 +675,9 @@ pub mod inbound_server_policies_server {
                 }
                 _ => {
                     Box::pin(async move {
-                        let mut response = http::Response::new(empty_body());
+                        let mut response = http::Response::new(
+                            tonic::body::Body::default(),
+                        );
                         let headers = response.headers_mut();
                         headers
                             .insert(
